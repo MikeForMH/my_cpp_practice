9a0f5db1de2e773b0ad94dcb3dba68ba5c988d1b
MikeForMH <michaelhuixu@gmail.com>
2018-12-21 00:37:23 +0800

    fix bug of copy constructor of pat 1004

--- class_practice/score_ranking_pat_1004/main.cpp
+++ class_practice/score_ranking_pat_1004/main.cpp
@@ -23,15 +23,6 @@ private:
     string _name;
     string _student_number;
     int _score;
-    int* get_score(){
-        return &_score;
-    }
-    string* get_name(){
-        return &_name;
-    }
-    string* get_number(){
-        return &_student_number;
-    }
 public:
     static vector <Student> _student_info;
     Student(const string &name, const string student_number, const int &score){
@@ -39,17 +30,24 @@ public:
         _student_number = student_number;
         _score = score;
         _student_info.push_back(*this);
+        ++_number_of_student;
         return;
     }
+    Student(const Student &obj){
+        _name = obj._name;
+        _student_number = obj._student_number;
+        _score = obj._score;
+        ++_number_of_student;
+    }
     ~Student(){
-        //--_number_of_student;
+        --_number_of_student;
     }
     static void quick_sort_ascending(vector <Student> &list, const int &left, const int &right){
         //cout << "testing 2" << endl;
         if(left < right){
-            int i = left, j = left, pivot = *list[right].get_score();
+            int i = left, j = left, pivot = list[right]._score;
             while(j < right){
-                if(*list[j].get_score() < pivot){
+                if(list[j]._score < pivot){
                     swap(list[i], list[j]);
                     ++i;
                 }
@@ -66,10 +64,7 @@ public:
         quick_sort_ascending(_student_info, 0, _number_of_student - 1);
     }
     static void output_student_info(const int &index){
-        cout << *_student_info[index].get_name() << " " << *_student_info[index].get_number() << endl;
-    }
-    static void add_count(){
-        ++_number_of_student;
+        cout << _student_info[index]._name << " " << _student_info[index]._student_number << endl;
     }
 };
 
@@ -83,7 +78,6 @@ int main(){
     for(int i = 0; i < number_of_student; ++i){
         cin >> student_name >> student_number >> score;
         Student(student_name, student_number, score);
-        Student::add_count();
     }
     Student::ascending_sort();
     Student::output_student_info(number_of_student - 1);
